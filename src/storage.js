cb08abc7ea8e214c031f5cda7f0db715af94c29c
kylepunta <kylepurcell1998@gmail.com>
2024-11-12 16:01:12 +0000

    Created storage file for local storage; added an array for storing the projects

---
+++ src/storage.js
@@ -0,0 +1,10 @@
+let projects = [10];
+projects[0] = "Monday";
+projects[1] = "Tuesday";
+projects[2] = "Wednesday";
+projects[3] = "Thursday";
+projects[4] = "Friday";
+projects[5] = "Saturday";
+projects[6] = "Sunday";
+
+export { projects };
