60c4465e1fc9eb8a623784057b713ceada45d753
kylepunta <kylepurcell1998@gmail.com>
2024-11-12 18:55:51 +0000

    Imported createNewListItem and listItems; added a for loop to initialize ten list items and displayed them

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import "./styles.css";
 import displayUI from "./UI.js";
+import { createNewListItem } from "./toDoItems.js";
+import { listItems } from "./storage.js";
 
 displayUI.displayHeader();
 displayUI.displaySidebar();
@@ -7,3 +9,10 @@ displayUI.displayContent();
 displayUI.displayMainHeading();
 displayUI.displaySidebarHeading();
 displayUI.displayProjects();
+displayUI.displayAddNewProject();
+
+for (let i = 0; i < 10; i++){
+    listItems[i] = createNewListItem("Book appointment", "Hair appointment", "14/11/2024", "High");
+};
+
+displayUI.displayListItems();
