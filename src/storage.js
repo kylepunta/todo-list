93e80549a2bf19881591f7ceeda7fa61b744ce5c
kylepunta <kylepurcell1998@gmail.com>
2024-11-14 11:26:29 +0000

    Imported createdNewListItem; added for loop to initialize ten list items

--- src/storage.js
+++ src/storage.js
@@ -1,3 +1,5 @@
+import { createNewListItem } from "./toDoItems.js";
+
 let projects = [8];
 projects[0] = "Monday";
 projects[1] = "Tuesday";
@@ -8,5 +10,8 @@ projects[5] = "Saturday";
 projects[6] = "Sunday";
 
 let listItems = [10];
+for (let i = 0; i < 10; i++){
+    listItems[i] = createNewListItem("Book appointment", "Hair appointment", "14/11/2024", "High");
+};
 
 export { projects, listItems };
