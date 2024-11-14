1cd017c0ffa5e5b1b7bb631ebd016e0bd0272012
kylepunta <kylepurcell1998@gmail.com>
2024-11-14 12:50:26 +0000

    Added svg for settings

--- src/UI.js
+++ src/UI.js
@@ -101,11 +101,17 @@ const displayUI = (function() {
             const priorityBoxes = document.createElement('div');
             priorityBoxes.classList.add('priority-boxes');
 
-            const svgString = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>chevron-down</title><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" /></svg>`;
-            const svgDoc = parser.parseFromString(svgString, "image/svg+xml").documentElement;
+            const svgStringOne = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>chevron-down</title><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" /></svg>`;
+            const svgDocOne = parser.parseFromString(svgStringOne, "image/svg+xml").documentElement;
             const expand = document.createElement('div');
             expand.classList.add('expand');
-            expand.appendChild(svgDoc);
+            expand.appendChild(svgDocOne);
+
+            const svgStringTwo = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>dots-horizontal</title><path d="M16,12A2,2 0 0,1 18,10A2,2 0 0,1 20,12A2,2 0 0,1 18,14A2,2 0 0,1 16,12M10,12A2,2 0 0,1 12,10A2,2 0 0,1 14,12A2,2 0 0,1 12,14A2,2 0 0,1 10,12M4,12A2,2 0 0,1 6,10A2,2 0 0,1 8,12A2,2 0 0,1 6,14A2,2 0 0,1 4,12Z" /></svg>`;
+            const settings = document.createElement('div');
+            settings.classList.add('settings');
+            const svgDocTwo = parser.parseFromString(svgStringTwo, "image/svg+xml").documentElement;
+            settings.appendChild(svgDocTwo);
 
             checklistMarkerContainer.appendChild(checklistMarker);
             titleContainer.appendChild(title);
@@ -123,6 +129,7 @@ const displayUI = (function() {
             listItemContainer.appendChild(dueDateContainer);
             listItemContainer.appendChild(priorityContainer);
             listItemContainer.appendChild(expand);
+            listItemContainer.appendChild(settings);
             content.appendChild(listItemContainer);
         });
     };
