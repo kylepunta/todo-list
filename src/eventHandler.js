81ca09d38405ef4fc3cf550977d9dbbfbf3a9de1
kylepunta <kylepurcell1998@gmail.com>
2024-12-02 15:53:58 +0000

    Added function to create a new list item which opens the dialog modal

--- src/eventHandler.js
+++ src/eventHandler.js
@@ -24,9 +24,70 @@ const eventHandler = (function() {
         };
     };
     
+    function createNewListItem() {
+        const content = document.querySelector('.content');
+        const dialog = document.querySelector('dialog');
+        dialog.classList.add('new-item-dialog');
+        const form = document.createElement('form');
+        form.classList.add('new-item-form');
 
-    return {expandListItems};
+        const mainHeading = document.createElement('h3');
+        mainHeading.textContent = "Add New List Item";
+      
+        const titleSection = document.createElement('div');
+        const descriptionSection = document.createElement('div');
+        const dateSection = document.createElement('div');
+        titleSection.classList.add('title-section');
+        descriptionSection.classList.add('description-section');
+        dateSection.classList.add('date-section');
+
+        const titleLabel = document.createElement('label');
+        const descriptionLabel = document.createElement('label');
+        const dateLabel = document.createElement('label');
+        titleLabel.textContent = "Title:";
+        descriptionLabel.textContent = "Description:";
+        dateLabel.textContent = "Date:";
+
+        const title = document.createElement('input');
+        const description = document.createElement('input');
+        const date = document.createElement('input');
+
+        const btnSection = document.createElement('div');
+        btnSection.classList.add('button-section');
+        const addBtn = document.createElement('button');
+        addBtn.addEventListener('click', (event) => {
+            event.preventDefault();
+        });
+        const cancelBtn = document.createElement('button');
+        cancelBtn.addEventListener('click', (event) => {
+            event.preventDefault();
+            dialog.close();
+        });
+        addBtn.classList.add('add-button');
+        cancelBtn.classList.add('cancel-button');
+        addBtn.textContent = "Add";
+        cancelBtn.textContent = "Cancel";
+        btnSection.appendChild(addBtn);
+        btnSection.appendChild(cancelBtn);
+
+        titleSection.appendChild(titleLabel);
+        titleSection.appendChild(title);
+        descriptionSection.appendChild(descriptionLabel);
+        descriptionSection.appendChild(description);
+        dateSection.appendChild(dateLabel);
+        dateSection.appendChild(date);
+
+        form.appendChild(mainHeading);
+        form.appendChild(titleSection);
+        form.appendChild(descriptionSection);
+        form.appendChild(dateSection);
+        form.appendChild(btnSection);
+        dialog.appendChild(form);
+        content.appendChild(dialog);
+        dialog.showModal();
+    };
+
+    return {expandListItems, createNewListItem};
 })();
-
 
 export { eventHandler };
