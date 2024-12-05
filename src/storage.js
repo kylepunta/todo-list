abd216c83be8bcb90dd0f4f45b24dff6ead40476
kylepunta <kylepurcell1998@gmail.com>
2024-12-05 14:10:23 +0000

    Removed code; created an array called projects and exported it

--- src/storage.js
+++ src/storage.js
@@ -1,17 +1,7 @@
 import { createNewListItem } from "./toDoItems.js";
 
-let projects = [8];
-projects[0] = "Monday";
-projects[1] = "Tuesday";
-projects[2] = "Wednesday";
-projects[3] = "Thursday";
-projects[4] = "Friday";
-projects[5] = "Saturday";
-projects[6] = "Sunday";
+const projects = [];
 
-let listItems = [10];
-for (let i = 0; i < 1; i++){
-    listItems[i] = createNewListItem("Book appointment", "Hair appointment", "14/11/2024", "High");
-};
+console.table(projects);
 
-export { projects, listItems };
+export { projects };
