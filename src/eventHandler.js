74cfd965544591409aeca31f2b717c1d34d8d0ec
kylepunta <kylepurcell1998@gmail.com>
2024-12-05 14:08:05 +0000

    Imported listItemsController; created function to add event listener to the add new list item button; created function to add event listeners to the add and cancel buttons in the dialogs; created function to add event listener to the add new project button

--- src/eventHandler.js
+++ src/eventHandler.js
@@ -1,7 +1,9 @@
+import { listItemsController } from "./toDoItems";
+
 const eventHandler = (function() {
     const parser = new DOMParser();
 
-    function expandListItems(expandBtns, descriptionContainers) {
+    function addExpandListItemsListeners(expandBtns, descriptionContainers) {
         for (let i = 0; i < expandBtns.length; i++){
             expandBtns[i].addEventListener('click', () => {
                 descriptionContainers[i].classList.toggle('expand-list-item');
@@ -23,71 +25,50 @@ const eventHandler = (function() {
             });
         };
     };
-    
-    function createNewListItem() {
-        const content = document.querySelector('.content');
-        const dialog = document.querySelector('dialog');
-        dialog.classList.add('new-item-dialog');
-        const form = document.createElement('form');
-        form.classList.add('new-item-form');
-
-        const mainHeading = document.createElement('h3');
-        mainHeading.textContent = "Add New List Item";
-      
-        const titleSection = document.createElement('div');
-        const descriptionSection = document.createElement('div');
-        const dateSection = document.createElement('div');
-        titleSection.classList.add('title-section');
-        descriptionSection.classList.add('description-section');
-        dateSection.classList.add('date-section');
-
-        const titleLabel = document.createElement('label');
-        const descriptionLabel = document.createElement('label');
-        const dateLabel = document.createElement('label');
-        titleLabel.textContent = "Title:";
-        descriptionLabel.textContent = "Description:";
-        dateLabel.textContent = "Date:";
-
-        const title = document.createElement('input');
-        const description = document.createElement('input');
-        const date = document.createElement('input');
-
-        const btnSection = document.createElement('div');
-        btnSection.classList.add('button-section');
-        const addBtn = document.createElement('button');
-        addBtn.addEventListener('click', (event) => {
-            event.preventDefault();
+    function addNewListItemBtnListener() {
+        const addNewListItem = document.querySelector('.add-new-list-item');
+        const addListItemDialog = document.querySelector('.add-list-item-dialog');
+        addNewListItem.addEventListener('click', () => {
+            addListItemDialog.showModal();
         });
-        const cancelBtn = document.createElement('button');
-        cancelBtn.addEventListener('click', (event) => {
-            event.preventDefault();
-            dialog.close();
-        });
-        addBtn.classList.add('add-button');
-        cancelBtn.classList.add('cancel-button');
-        addBtn.textContent = "Add";
-        cancelBtn.textContent = "Cancel";
-        btnSection.appendChild(addBtn);
-        btnSection.appendChild(cancelBtn);
-
-        titleSection.appendChild(titleLabel);
-        titleSection.appendChild(title);
-        descriptionSection.appendChild(descriptionLabel);
-        descriptionSection.appendChild(description);
-        dateSection.appendChild(dateLabel);
-        dateSection.appendChild(date);
-
-        form.appendChild(mainHeading);
-        form.appendChild(titleSection);
-        form.appendChild(descriptionSection);
-        form.appendChild(dateSection);
-        form.appendChild(btnSection);
-        dialog.appendChild(form);
-        content.appendChild(dialog);
-        dialog.showModal();
     };
 
-    return {expandListItems, createNewListItem};
+    function addListItemDialogListeners() {
+        const addListItemBtn = document.querySelector('.add-list-item-button');
+        const cancelListItemBtn = document.querySelector('.cancel-list-item-button');
+        const addListItemDialog = document.querySelector('.add-list-item-dialog');
+
+        addListItemBtn.addEventListener('click', (event) => {
+            event.preventDefault();
+        });
+        cancelListItemBtn.addEventListener('click', (event) => {
+            event.preventDefault();
+            addListItemDialog.close();
+        });
+    };
+    function addProjectBtnListener() {
+        const addNewProjectBtn = document.querySelector('.add-new-project-button');
+        const addProjectDialog = document.querySelector('.add-project-dialog');
+        addNewProjectBtn.addEventListener('click', () => {
+            addProjectDialog.showModal();
+        });
+    };
+
+    function addProjectDialogListeners() {
+        const addProjectBtn = document.querySelector('.add-project-button');
+        const cancelProjectBtn = document.querySelector('.cancel-project-button');
+        const addProjectDialog = document.querySelector('.add-project-dialog');
+
+        addProjectBtn.addEventListener('click', (event) => {
+            event.preventDefault();
+        });
+        cancelProjectBtn.addEventListener('click', (event) => {
+            event.preventDefault();
+            addProjectDialog.close();
+        });
+    };
+
+    return {addExpandListItemsListeners, addListItemDialogListeners, addNewListItemBtnListener, addProjectBtnListener, addProjectDialogListeners};
 })();
 
 export { eventHandler };
