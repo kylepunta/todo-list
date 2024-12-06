3367646cefede352eea79fba2d9428197b01749e
kylepunta <kylepurcell1998@gmail.com>
2024-12-06 13:07:18 +0000

    Created JS module called state to track the current project and created getter and setter function for the state variable currentProject

---
+++ src/state.js
@@ -0,0 +1,10 @@
+let currentProject = null;
+
+function setCurrentProject(project) {
+    currentProject = project;
+};
+function getCurrentProject() {
+    return currentProject;
+};
+
+export {setCurrentProject, getCurrentProject};
--- src/toDoItems.js
+++ src/toDoItems.js
@@ -1,15 +1,27 @@
+import { getCurrentProject, setCurrentProject } from "./state";
+import { projects } from "./storage";
+import displayUI from "./UI";
+
 const listItemsController = (function() {
+    function createListItem(title, description, date) {
+        const listItem = {
+            title: title,
+            description: description,
+            date: date,
+        };
+        return listItem;
+    };
     function addListItem() {
         const title = document.querySelector('.title').value;
         const description = document.querySelector('.description').value;
         const date = document.querySelector('.date').value;
+
+        let listItem = createListItem(title, description, date);
+        let currentProject = getCurrentProject();
+        projects[currentProject].listItems.push(listItem);
+        displayUI.displayProject();
     };
-    
-    function createNewListItem() {
-        const addListItemdialog = document.querySelector('.add-list-item-dialog');
-        addListItemdialog.showModal();
-    };
-    return {addListItem, createNewListItem};    
+    return { createListItem, addListItem };    
 })();
 
 
