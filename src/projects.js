87df2f946085b5edcc7cdb2214f433fe029ae391
kylepunta <kylepurcell1998@gmail.com>
2024-12-06 13:04:44 +0000

    Imported functions; added functions to create project and add project to the storage

--- src/projects.js
+++ src/projects.js
@@ -1,6 +1,21 @@
+import { projects } from "./storage.js";
+import displayUI from "./UI.js";
+
 const projectsController = (function() {
-    function createNewProject() {
-        const addProjectDialog = document.querySelector('.add-project-dialog');
-        addProjectDialog.showModal();
+    function createProject(title) {
+        const project = {
+            title: title,
+            listItems: [],
+        };
+        return project;
+    }
+    function addProject() {
+        const projectTitle = document.querySelector('.project-title').value;
+        let project = createProject(projectTitle);
+        projects.push(project);
+        console.log(projects);
+        displayUI.displayProjects();
     };
+    return {createProject, addProject};
 })();
+export {projectsController};
