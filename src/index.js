cf215384a0fb4bc86091d4b8675f0e5e5ab1689a
kylepunta <kylepurcell1998@gmail.com>
2024-12-12 13:59:31 +0000

    Removed unused imports and redundant code

--- src/index.js
+++ src/index.js
@@ -1,14 +1,6 @@
 import "./styles.css";
 import displayUI from "./UI.js";
 import { eventHandler } from "./eventHandler.js";
-import { projectsController } from "./projects.js";
-import { projects } from "./storage.js";
-import { setCurrentProject } from "./state.js";
-
-let defaultProject = projectsController.createProject("Untitled");
-projects.push(defaultProject);
-setCurrentProject(0);
-console.table(projects);
 
 displayUI.displayHeader();
 displayUI.displaySidebar();
@@ -18,18 +10,12 @@ displayUI.displaySidebarHeading();
 displayUI.displayProjects();
 displayUI.displayAddNewProject();
 displayUI.displayAddListItemDialog();
-displayUI.displayProjectNameHeading();
-displayUI.displayAddNewListItem();
 displayUI.displayAddProjectDialog();
 displayUI.displayDeleteDialog();
 displayUI.displayRenameDialog();
 displayUI.displayEditDialog();
 displayUI.displayProjectSettings();
 
-const expandBtns = document.querySelectorAll('.expand');
-const descriptionContainers = document.querySelectorAll('.description-container');
-
-eventHandler.addNewListItemBtnListener();
 eventHandler.addListItemDialogListeners();
 eventHandler.addProjectBtnListener();
 eventHandler.addProjectDialogListeners();
